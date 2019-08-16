88ed0a41a5826f089a22e1f9bc7f5e1d5b783fc3
Feng Feng <ffeng@bu.edu>
2019-08-16 15:00:53 -0400

    add new comment to explain the divergence score

--- R/cWrapper.R
+++ R/cWrapper.R
@@ -3,7 +3,7 @@
 #'@export
 #'@useDynLib rcPkg add_rc
 #
-add_r<-function(x,y)
+add_r_w<-function(x,y)
 {
 	.C(add_rc, x, y, numeric(1))[[3]]
 }
@@ -15,7 +15,7 @@ add_r<-function(x,y)
 
 #'@export
 #'@useDynLib rcPkg add_rc_call
-add_rcall<-function(x,y)
+add_rcall_w<-function(x,y)
 {
 	.Call(add_rc_call, x, y)
 }
@@ -30,7 +30,7 @@ add_rcall<-function(x,y)
 #'@export
 #'@useDynLib rcPkg subsetFullCombination_c
 #
-subsetFullCombination<-function(x,y)
+subsetFullCombination_w<-function(x,y)
 {
 	.Call(subsetFullCombination_c, x, y, round(factorial(x)/factorial(y)/factorial(x-y)))
 }
@@ -38,16 +38,18 @@ subsetFullCombination<-function(x,y)
 #'@title R wrapper of c function to search for the diverse barcodes
 #'@description it passes in the barcodes and indexes of barcodes. The called
 #'	c function will search all index group for diverse/balanced barcodes.
+#'@details the divergence score is calculated as sum^2(freq-c(0.25,0.25,0.25,0.25)) for 
+#'	positions and then normalized by sequence length.
 #'@param s character matrix, assuming its each is for one barcodes sequences and 
 #'	columns are the positions of nucleotide in the barcode sequence.
 #'@param index numeric matrix, holding the combinations of indexes to search from the barcode matrix above.
 #'	it is arranged similiar to the output from the function \code{\link{subsetFullCombination}}. Each column
 #'	is for one subset of index to be seach for diverse barcodes.
-#'@return a list of index combinations will give us the balanced barcodes.
+#'@return a list of index combinations will give us the balanced barcodes and also the divergence score
 #'
 #'@export
 #'@useDynLib rcPkg getDiverseSet_c
-getDiverseSet<-function(s,  index)
+getDiverseSet_w<-function(s,  index)
 {
 	#cat("dim",dim(s)[1]);
 	.Call(getDiverseSet_c, s, dim(s)[1], dim(s)[2],index, dim(index)[2], dim(index)[1]
